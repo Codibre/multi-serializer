46643de6384fb6886d42e1da216c44a123f36c96
Thiago Santos <tos.oliveira@gmail.com>
2021-06-23 00:27:33 -0300

    Simplifying Serializer code

--- src/serializer/index.ts
+++ src/serializer/index.ts
@@ -15,28 +15,18 @@ export class Serializer<A> {
 	}
 
 	async serialize<T extends A>(data: T): Promise<Serialized> {
-		return this.chainSerialize(data, 0);
-	}
-
-	private async chainSerialize<T>(
-		content: T,
-		index: number,
-	): Promise<Serialized> {
-		const result = await this.strategies[index].serialize(content);
-
-		return index < this.strategies.length - 1
-			? this.chainSerialize(result, index + 1)
-			: result;
+		let result: any = data;
+		for (let i = 0; i < this.strategies.length; i++) {
+			result = await this.strategies[i].serialize(result);
+		}
+		return result;
 	}
 
 	async deserialize<T extends A>(data: Serialized): Promise<T> {
-		return this.chainDeserialize(data, this.strategies.length - 1);
-	}
-
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	private async chainDeserialize(content: any, index: number): Promise<any> {
-		const result = await this.strategies[index].deserialize(content);
-
-		return index > 0 ? this.chainDeserialize(result, index - 1) : result;
+		let result: any = data;
+		for (let i = this.strategies.length - 1; i >= 0; i--) {
+			result = await this.strategies[i].deserialize(result);
+		}
+		return result;
 	}
 }
