be5f3cce888da21853a6caa3691d40491c269a4a
Thiago Santos <tos.oliveira@gmail.com>
2021-06-23 00:19:46 -0300

    removing unused file

--- src/types/index.ts
+++
@@ -1,2 +0,0 @@
-/* istanbul ignore file */
-export { TransformStrategy } from './transform-strategy';
--- src/types/transform-strategy.ts
+++
@@ -1,5 +0,0 @@
-/* istanbul ignore file */
-export enum TransformStrategy {
-	SERIALIZE = 'serialize',
-	DESERIALIZE = 'deserialize',
-}
