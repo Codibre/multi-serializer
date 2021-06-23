5b25b52c3dc27a8c5ad6081a56cf70ba0eca2af9
Thiago Santos <tos.oliveira@gmail.com>
2021-06-23 19:31:29 -0300

    Making GzipStrategy ignore non gzip input

--- test/unit/serializer/index.spec.ts
+++ test/unit/serializer/index.spec.ts
@@ -126,6 +126,22 @@ describe('index.ts', () => {
 		expect(read).toMatchObject(req);
 	});
 
+	it('should work with json serializing and json + gzip deserializing (as no gzip buffers are ignored by GzipStrategy)', async () => {
+		const req = {
+			bar: 'abc',
+		};
+		const serializer = new Serializer(new JsonStrategy<typeof req>({}));
+		const deserializer = new Serializer(
+			new JsonStrategy<typeof req>({}),
+			new GzipStrategy(),
+		);
+
+		const write = await serializer.serialize(req);
+		const read = await deserializer.deserialize(write);
+
+		expect(read).toMatchObject(req);
+	});
+
 	it('should work with json + gzip + gzip', async () => {
 		const req = {
 			bar: 'abc',
